cce7134a22b1da4a627d50a43fa384ab0633f1b2
CJY <jim890112che@gmail.com>
2023-12-22 16:12:12 +0800

    Create best solution.py

---
+++ 0001~0500/242. Valid Anagram/best solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        tem = set(s)
+        for i in tem:
+            if not (i in t and i in s and s.count(i) == t.count(i)):
+                return False
+        return True
