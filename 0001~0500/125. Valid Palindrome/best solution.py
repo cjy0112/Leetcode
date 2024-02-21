e09a4088dfd150f58484cf2c581351f78fbcd940
CJY <jim890112che@gmail.com>
2024-02-21 11:32:04 +0800

    Create best solution.py

---
+++ 0001~0500/125. Valid Palindrome/best solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s=s.lower()
+        pattern =r'[^a-z0-9]+'
+        a = re.sub(pattern,'',s)
+        
+        if(a[::-1]==a):
+            return True
+        else:
+            return False
+        
+# Best Runtime
